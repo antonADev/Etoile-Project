2d17fb041c618ff258b7b0dcaf11d633fc1a551d
antonADev <antonadeveloper@gmail.com>
2021-11-16 17:19:51 +0100

    Added index.html, style.css, main.js and image files
    Setting up this new project

---
+++ main.js
@@ -0,0 +1,29 @@
+const toggleButton = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.navbar-links');
+const navBar = document.querySelector('[data-nav]');
+
+toggleButton.addEventListener('click', () => {
+    if (navLinks.classList.contains('show-links')){
+        navLinks.classList.remove('show-links');
+    } else {
+        navLinks.classList.add('show-links');
+    }
+})
+
+// window.addEventListener('scroll', (event) => {
+
+//     const scrollTop = window.scrollY;
+//     var navResizeFunc = function () {
+//         let height;
+    
+//         if (scrollTop <= 50) {
+//           height = 100 - scrollTop;
+//         } else {
+//           height = 50;
+//         }
+//         navBar.style.height = `${height}px`;
+//       };
+    
+//       navResizeFunc(true);
+//       window.addEventListener("scroll", navResizeFunc());
+// })
